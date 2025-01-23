95af1136f2b5b4276fc58b4777747ddd02847bd5
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-01-23 23:14:21 +0200

    done

--- lib/core/language/app_localizations.dart
+++ lib/core/language/app_localizations.dart
@@ -23,7 +23,7 @@ class AppLocalizations {
   // Method to load the localized strings from JSON files.
   Future<void> load() async {
     final jsonString =
-        await rootBundle.loadString('lang/${locale.languageCode}.json');
+        await rootBundle.loadString('langs/${locale.languageCode}.json');
 
     final jsonMap = json.decode(jsonString) as Map<String, dynamic>;
 
