aaa462e78a1e6066869d77755ffdbe06c9915741
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-01-23 23:15:04 +0200

    colors light

---
+++ lib/core/styles/colors/colors_light.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsLight {
+  static const Color mainColor = Color(0xFF233B55);
+  static const Color secondaryColor = Color(0xFF336EA6);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
