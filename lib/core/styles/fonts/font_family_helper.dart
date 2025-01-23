1473d2f9f6728a1985483b63fbf17ce67013901a
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-01-24 00:24:02 +0200

    create family helper in styles in theme

--- lib/core/styles/fonts/font_family_helper.dart
+++ lib/core/styles/fonts/font_family_helper.dart
@@ -2,18 +2,19 @@ import 'package:markaz_elamal/core/services/shared_pref/pref_keys.dart';
 import 'package:markaz_elamal/core/services/shared_pref/shared_pref.dart';
 
 class FontFamilyHelper {
-  const FontFamilyHelper._();
+  // const FontFamilyHelper._();
 
   static const String cairoArabic = 'Cairo';
 
   static const String poppinsEnglish = 'Poppins';
+  static const String peraltaEnglish = 'Peralta';
 
   static String geLocalozedFontFamily() {
     final currentLanguage = SharedPref().getString(PrefKeys.language);
     if (currentLanguage == 'ar') {
-      return cairoArabic;
+      return peraltaEnglish;
     } else {
-      return poppinsEnglish;
+      return peraltaEnglish;
     }
   }
 }
