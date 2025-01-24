a64ee6fc8174bb772678f5d02cf02e7bd888ae35
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-01-24 16:01:34 +0200

    add app logo svg image

--- lib/core/styles/images/app_images.dart
+++ lib/core/styles/images/app_images.dart
@@ -3,6 +3,8 @@ class AppImages {
   static const noNetwork = 'assets/images/core/no_network.jpg';
   static const pageUnderBuild = 'assets/images/core/page_under_build.jpg';
   static const emptyScreen = 'assets/images/core/empty_screen.png';
+  static const appLogo = 'assets/images/logo.png';
+  static const appLogoSVG = 'assets/images/logo.svg';
   static const String onboarding1 = 'assets/images/patient/onboarding1.png';
   static const String onboarding2 = 'assets/images/patient/onboarding2.png';
   static const String onboarding3 = 'assets/images/patient/onboarding3.png';
